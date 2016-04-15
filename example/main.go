e2bcc2b8b5e1ff98378921d72061f8fc0f0d0ccc
Will Dixon <will.dixon@acstechnologies.com>
2016-04-15 15:55:17 -0400

    revised example

--- example/main.go
+++ example/main.go
@@ -1,13 +1,33 @@
 package main
 
-import "github.com/dixonwille/test/env"
+import (
+	"os"
+	"runtime"
+
+	"github.com/dixonwille/wlog"
+)
+
+//UI is used to print to screen
+var UI wlog.UI
+
+func init() {
+	var prefixed wlog.UI
+	basic := wlog.New(os.Stdin, os.Stdout, os.Stderr)
+	if runtime.GOOS != "windows" {
+		prefixed = wlog.AddPrefix("", "", "["+wlog.Check+"]", "", "["+wlog.Cross+"]", "[!]", "[~]", basic)
+	} else {
+		prefixed = wlog.AddPrefix("", "", "[$]", "", "[x]", "[!]", "[~]", basic)
+	}
+	colored := wlog.AddColor(wlog.None, wlog.None, wlog.BrightGreen, wlog.BrightCyan, wlog.BrightRed, wlog.BrightYellow, wlog.BrightMagenta, prefixed)
+	UI = wlog.AddConcurrent(colored)
+}
 
 func main() {
-	env.UI.Log("Log Statement")
-	env.UI.Output("Output Statement")
-	env.UI.Running("Running Statement")
-	env.UI.Success("Success Statement")
-	env.UI.Info("Info Statement")
-	env.UI.Warn("Warning Statement")
-	env.UI.Error("Error statement")
+	UI.Log("Log Statement")
+	UI.Output("Output Statement")
+	UI.Running("Running Statement")
+	UI.Success("Success Statement")
+	UI.Info("Info Statement")
+	UI.Warn("Warning Statement")
+	UI.Error("Error statement")
 }
